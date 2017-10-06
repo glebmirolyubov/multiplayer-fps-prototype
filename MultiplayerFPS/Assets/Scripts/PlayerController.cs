c192d6e715ae4d30f1e28d8a15b34471dae589c6
Gleb Mirolyubov <gleb.mirolyubov@gmail.com>
2017-10-06 22:40:24 +0100

    Add Player Model and Animations with Particle Effects

--- MultiplayerFPS/Assets/Scripts/PlayerController.cs
+++ MultiplayerFPS/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(ConfigurableJoint))]
 [RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour {
@@ -20,13 +21,16 @@ public class PlayerController : MonoBehaviour {
     [SerializeField]
     private float jointMaxForce = 40f;
 
+    //Component caching
     private PlayerMotor motor;
     private ConfigurableJoint joint;
+    private Animator animator;
 
     private void Start()
     {
         motor = GetComponent<PlayerMotor>();
         joint = GetComponent<ConfigurableJoint>();
+        animator = GetComponent<Animator>();
 
         SetJointSettings(jointSpring);
     }
@@ -34,14 +38,17 @@ public class PlayerController : MonoBehaviour {
     private void Update()
     {
         //Calculate our movement velocity as 3D Vector
-        float _xMov = Input.GetAxisRaw("Horizontal");
-        float _zMov = Input.GetAxisRaw("Vertical");
+        float _xMov = Input.GetAxis("Horizontal");
+        float _zMov = Input.GetAxis("Vertical");
 
         Vector3 _movHorizontal = transform.right * _xMov;
         Vector3 _movVertical = transform.forward * _zMov;
 
         // Final movement vector
-        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+        Vector3 _velocity = (_movHorizontal + _movVertical) * speed;
+
+        //Animate movement
+        animator.SetFloat("ForwardVelocity", _zMov);
 
         // Apply movement
         motor.Move(_velocity);
