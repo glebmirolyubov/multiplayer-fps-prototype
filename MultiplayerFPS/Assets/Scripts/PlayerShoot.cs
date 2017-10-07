bb2a8a2b4613c52cc0c39a53dbe7024261f59e2a
Gleb Mirolyubov <gleb.mirolyubov@gmail.com>
2017-10-07 14:41:56 +0100

    Add Rapid Fire mechanics

--- MultiplayerFPS/Assets/Scripts/PlayerShoot.cs
+++ MultiplayerFPS/Assets/Scripts/PlayerShoot.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 
+[RequireComponent (typeof(WeaponManager))]
 public class PlayerShoot : NetworkBehaviour {
 
     private const string PLAYER_TAG = "Player";
@@ -11,15 +12,8 @@ public class PlayerShoot : NetworkBehaviour {
     [SerializeField]
     private LayerMask mask;
 
-    [SerializeField]
-    private PlayerWeapon weapon;
-
-    [SerializeField]
-    private GameObject weaponGFX;
-
-	[SerializeField]
-	private string weaponLayerName = "Weapon";
-
+    private PlayerWeapon currentWeapon;
+    private WeaponManager weaponManager;
 
 	private void Start()
     {
@@ -28,14 +22,26 @@ public class PlayerShoot : NetworkBehaviour {
             this.enabled = false;
         }
 
-        weaponGFX.layer = LayerMask.NameToLayer(weaponLayerName);
+        weaponManager = GetComponent<WeaponManager>();
     }
 
     private void Update()
     {
-        if (Input.GetButtonDown("Fire1")) {
-            Shoot();
-        }
+        currentWeapon = weaponManager.GetCurrentWeapon();
+
+        if (currentWeapon.fireRate <= 0f) {
+			if (Input.GetButtonDown("Fire1"))
+			{
+				Shoot();
+			}
+        } else {
+            if (Input.GetButtonDown("Fire1")) {
+                InvokeRepeating("Shoot", 0f, 1f/currentWeapon.fireRate);
+            } else if (Input.GetButtonUp("Fire1")) {
+                CancelInvoke("Shoot");
+            }
+        } 
+
     }
 
     [Client]
@@ -43,10 +49,10 @@ public class PlayerShoot : NetworkBehaviour {
     {
         RaycastHit _hit;
 
-        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, weapon.range, mask))
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, currentWeapon.range, mask))
         {
             if (_hit.collider.tag == PLAYER_TAG) {
-                CmdPlayerShot(_hit.collider.name, weapon.damage);
+                CmdPlayerShot(_hit.collider.name, currentWeapon.damage);
             }
         }
     }
